a675b49c888be6fba95e2c4b04ce5a39d5f654fc
Dongsun Oh <dongsun.oh@gmail.com>
2019-05-10 22:33:01 +0200

    added Middlewares (morgan, helmet, body-parser, cookie-parser)

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // const express = require("express");
+
+// babel 사용시 아래 구문 사용 가능
 import express from "express";
-const app = express();
+import morgan from "morgan"; // log 사용 (npm morgan install require)
+import helmet from "helmet";  // 보안을 위한 것들 (앱의 보안)
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const  app = express();
 
 const PORT = 4000;
 
@@ -13,15 +20,20 @@ const handleHome = (req, res) => res.send("Hello from my ass");
 const handleProfile = (req, res) => res.send("You are on my profile");
 
 // Middlewares
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  // next function call
-  next();
-};
+// const betweenHome = (req, res, next) => {
+//   console.log("Between");
+//   // next function call
+//   next();
+// };
 
 // route선언 전 middleware 사용해야 모든 라우트에 적용 됨. (순서 중요함.)
 // middleware global set
-app.use(betweenHome);
+// app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(helmet());  // 보안 설정
+app.use(morgan("dev"));  // morgan log 사용
 
 // route define
 app.get("/", handleHome);
